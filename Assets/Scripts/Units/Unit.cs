18374d940ad26c5eae04b775731064e9ca5cba60
Wilkinson Da Rolt de Souza <wilki.darolt@gmail.com>
2023-03-02 01:06:44 -0300

    Add option to client and server have a list of their Units.
    This reffers to lesson 20.

--- Assets/Scripts/Units/Unit.cs
+++ Assets/Scripts/Units/Unit.cs
@@ -1,4 +1,5 @@
 using Mirror;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,12 +11,46 @@ public class Unit : NetworkBehaviour
     [SerializeField] private UnityEvent onSelected = null;
     [SerializeField] private UnityEvent onDeselected = null;
 
+    public static event Action<Unit> ServerOnUnitSpawned;
+    public static event Action<Unit> ServerOnUnitDespawned;
+
+    public static event Action<Unit> AuthorityOnUnitSpawned;
+    public static event Action<Unit> AuthorityOnUnitDespawned;
+
     public UnitMovement GetUnitMovement()
     {
         return unitMovement;
     }
 
+    #region Server
+    public override void OnStartServer(){
+        ServerOnUnitSpawned?.Invoke(this);
+    }
+
+    public override void OnStopServer(){
+        ServerOnUnitDespawned?.Invoke(this);
+    }
+
+    #endregion
+
     #region Client
+
+    public override void OnStartClient(){
+        if (!isClientOnly || !isOwned) {
+            return;
+        }
+
+        AuthorityOnUnitSpawned?.Invoke(this);
+    }
+
+    public override void OnStopClient(){
+        if (!isClientOnly || !isOwned) {
+            return;
+        }
+
+        AuthorityOnUnitDespawned?.Invoke(this);
+    }
+
     [Client]
     public void Select()
     {
